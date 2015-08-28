baf2a305e67e399d269d593201efd823c9c6423c
Rohtash Poonia <rohtash.poonia@sap.com>
2015-08-28 13:22:55 +0530

    authentication
    Change-Id: I3a7e861365ecfe75e6c2b3b6610fc78a1e28cff6

--- config/auth.js
+++ config/auth.js
@@ -26,19 +26,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '840615919378962', // your App ID
         'clientSecret'    : '43dbc5a94cd87168d05ee5440b24640c', // your App Secret
-        'callbackURL'     : 'https://trendingapi.herokuapp.com/auth/callback'
+        'callbackURL'     : 'https://jhansal.herokuapp.com/auth/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'        : 'UfkfsU4h7j5flFekw8Nl8OxFG',
         'consumerSecret'     : 'qxtP2Tjkp4ZNxR3C9N3zDqeuY5xUTf2zZmINO2k4oOOYPTJ1dA',
-        'callbackURL'        : 'https://trendingapi.herokuapp.com/auth/twitter/callback'
+        'callbackURL'        : 'https://jhansal.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID'         : '632639167210-fsvm20k0f00inlimbfienoenbdci42f0.apps.googleusercontent.com',
-        'clientSecret'     : 'MOVM2Cps7uzWIWmqYfww8EqF',
-        'callbackURL'      : 'https://trendingapi.herokuapp.com/oauth2/callback'
+        'clientID'         : '632639167210-9ri4jqvei480u15t6tc6mjamse5dktm6.apps.googleusercontent.com',
+        'clientSecret'     : '0KOpgUuUUxc1ThtEyL8XxkJF',
+        'callbackURL'      : 'https://jhansal.herokuapp.com/oauth2/callback'
     }
 
 };
